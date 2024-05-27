290615402e1d633eec9e0eb5d9592dd48df9ad24
ChemicaLS061 <chemicals061@gmail.com>
2024-05-27 21:52:12 +0500

    Update (fix bugs)

--- src/Main.java
+++ src/Main.java
@@ -1,50 +1,65 @@
 import javax.swing.plaf.ScrollPaneUI;
+import java.io.IOException;
+import java.nio.channels.FileLock;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner s = new Scanner(System.in);
         String arithOper = s.nextLine();
         String answer = calc(arithOper);
         System.out.println(answer);
     }
 
-    public static String calc (String inOper) {
+    public static String calc (String inOper) throws IOException {
         int cal = 0;
         boolean flagRim = false;
         String outOper = "";
-        inOper = inOper.trim();
-        if(inOper.isEmpty()){}
+        inOper = inOper.replaceAll(" ", "");
+        inOper = inOper.replaceAll("\t", "");
+        if(inOper.isEmpty()){
+            throw new IOException();
+        }
         inOper = inOper.toUpperCase();
         int aCode = inOper.codePointAt(0);
         if(aCode == 73 || aCode == 86 || aCode == 88){
             outOper = rim(inOper);
-        }
-        if(aCode >= 48 && aCode <= 57) arab(inOper);
-//        try {
-//            cal = Integer.parseInt(outOper);
-//        } catch (NumberFormatException nfe){
-//            System.out.println("NumberFormatException: " + nfe.getMessage());
-//        }
-
+        } else if(aCode >= 48 && aCode <= 57) outOper = String.valueOf(arab(inOper));
+        else throw new IllegalArgumentException("Введите числа в диапозоне 1-10");
         return outOper;
     }
 
-    public static void arab (String inOper) {
+    public static int arab (String inOper) throws IOException {
+        int a = 0;
+        int b = 0;
+        int sum = 0;
         boolean flag0 = false;
         boolean flag1 = false;
         boolean flag2 = false;
         char sign = '0';
-        for (int i = 0; inOper.codePointAt(i) != 0; i++) {
+        char number;
+        for (int i = 0; i < inOper.length(); i++) {
             int aCode = inOper.codePointAt(i);
             if (!flag1 && !flag0 && !flag2) {
-                if (aCode == 49) flag1 = true;
-                if (aCode > 49 && aCode <= 57) flag2 = true;
+                if (aCode == 49) {
+                    flag1 = true;
+                    if(sign == '0') a = 1;
+                    else b = 1;
+                } else if (aCode > 49 && aCode <= 57) {
+                    number = (char) aCode;
+                    flag2 = true;
+                    if(sign == '0') a = Integer.parseInt(String.valueOf(number));
+                    else b = Integer.parseInt(String.valueOf(number));
+                } else {
+                    throw new IllegalArgumentException("Введите числа в диапозоне 1-10");
+                }
             } else if (aCode == 48 && !flag0 && !flag2) {
                 flag1 = false;
                 flag0 = true;
+                if(sign == '0') a = 10;
+                else b = 10;
             } else if(sign == '0'){
                 switch (aCode) {
                     case 42:
@@ -59,22 +74,29 @@ public class Main {
                     case 47:
                         sign = '/';
                         break;
-                    default: System.out.println("Некорректный ввод");       //Изменить на ошибку
+                    default: throw new IOException();
                 }
                 flag0 = false;
                 flag1 = false;
                 flag2 = false;
-            } else System.out.println("Некорректный ввод");     //Изменить на ошибку
+            } else throw new IOException();
         }
+        switch (sign) {
+            case '+': sum = a+b; break;
+            case  '*': sum = a*b; break;
+            case '-': sum = a-b; break;
+            case '/': sum = a/b; break;
+        }
+        return sum;
     }
 
-    public static String rim (String inOper) {
+    public static String rim (String inOper) throws IOException {
         int a = 0;
         int b = 0;
         int sum = 0;
         byte flagI = 0;
-        boolean flagV = false;
-        boolean flagX = false;
+        byte flagV = 0;
+        byte flagX = 0;
         char sign = '0';
         int i = 0;
         String outOper = "";
@@ -101,12 +123,12 @@ public class Main {
             for (String element: strings) {
                 for (i = 0; i < element.length(); i++) {
                     int aCode = element.codePointAt(i);
-                    if (flagI < 4 && !flagV && !flagX) {
+                    if (flagI < 4 && flagV < 2 && flagX < 2) {
                         if (aCode == 73 && flagI != 3) {
                             flagI += 1;}
-                    if (aCode == 86 && flagI <= 1) flagV = true;
-                    if (aCode == 88 && flagI <= 1) flagX = true;
-                    } else System.out.println("Некорректный ввод3");     //Изменить на ошибку
+                    if (aCode == 86 && flagI <= 1) flagV += 1;
+                    if (aCode == 88 && flagI <= 1) flagX += 1;
+                    } else throw new IOException();    //Изменить на ошибку
                 }
                 if(a!=0) {
                     b += convertRimArab(element);
@@ -114,8 +136,8 @@ public class Main {
                     a += convertRimArab(element);
                 }
                 flagI = 0;
-                flagV = false;
-                flagX = false;
+                flagV = 0;
+                flagX = 0;
             }
             switch (sign) {
                 case '+': sum = a+b; break;
@@ -130,20 +152,25 @@ public class Main {
     public static int convertRimArab(String inOper){
         int outOper = 0;
         int answer = 0;
+        boolean flagSum = false;
         if(inOper.length() > 1) {
             for (int i = inOper.length() - 1; i > 0; i--) {
-                if ((byte) inOper.charAt(i - 1) >= (byte) inOper.charAt(i)) {
-                    Converting conv = Converting.valueOf(String.valueOf(inOper.charAt(i - 1)));
-                    outOper += conv.getValue();
-                    conv = Converting.valueOf(String.valueOf(inOper.charAt(i)));
-                    outOper += conv.getValue();
-                }
                 if (inOper.charAt(i - 1) < inOper.charAt(i)) {
                     Converting conv = Converting.valueOf(String.valueOf(inOper.charAt(i)));
                     answer += conv.getValue();
                     conv = Converting.valueOf(String.valueOf(inOper.charAt(i - 1)));
                     outOper += answer - conv.getValue();
                 }
+                if ((byte) inOper.charAt(i - 1) >= (byte) inOper.charAt(i)) {
+                    if(!flagSum) {
+                        Converting conv = Converting.valueOf(String.valueOf(inOper.charAt(i)));
+                        outOper += conv.getValue();
+                    }
+                    Converting conv = Converting.valueOf(String.valueOf(inOper.charAt(i - 1)));
+                    outOper += conv.getValue();
+                    flagSum = true;
+                }
+
             }
         } else {
             Converting conv = Converting.valueOf(String.valueOf(inOper.charAt(0)));
@@ -155,7 +182,7 @@ public class Main {
 
     public static String convertArabRim(int number) {
         if ((number <= 0) || (number > 100)) {
-            throw new IllegalArgumentException(number + " is not in range (0,100]");
+            throw new IllegalArgumentException(number + " число находится за пределами диапазона (0,100]");
         }
         String outOper = "";
         Converting s = Converting.C;
